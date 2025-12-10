e8f89ecbcefdb8ff88a0468d86703bfc13aad39b
hazel0209 <karisk0209@gmail.com>
2025-12-10 16:35:33 +0900

    dev

--- js/custom.js
+++ js/custom.js
@@ -10,49 +10,64 @@ $(function () {
   //     });
   //   }, 2000);
 
-  //   setInterval(() => {
-  //     $(".mus_thumb").animate({ left: "-30vw" }, 2000, function () {
-  //       $(".mus_thumb > li:first-child").appendTo(".mus_thumb");
-  //       $(".mus_thumb").css({ left: "0" });
-
-  //       $(".mus_thumb > li").removeClass("on");
-  //       $(".mus_thumb > li:nth-child(2)").addClass("on");
-  //     });
-  //   }, 3000);
-
   let i = 0;
   let musicminute = document.querySelector(".mus_num");
 
-  let stop = setInterval(() => {
+  setInterval(() => {
     if (i > 2) {
       i = 0;
     } else {
       i++;
     }
-    musicminute.textContent = `${i}`;
 
-    $(".mus_thumb").css({
-      transform: "translateX(-30vw)",
-      transition: "transform 1s ease",
-    });
-
-    // 1초 후 실행 (transition 시간과 맞춤)
-    setTimeout(() => {
+    $(".mus_thumb").animate({ left: "-30vw" }, 2000, function () {
       $(".mus_thumb > li:first-child").appendTo(".mus_thumb");
-      $(".mus_thumb").css({
-        transform: "translateX(0)",
-        transition: "",
-      });
+      $(".mus_thumb").css({ left: "0" });
 
-      // .on 클래스 이동
       $(".mus_thumb > li").removeClass("on");
       $(".mus_thumb > li:nth-child(2)").addClass("on");
+      musicminute.textContent = `${i}`;
       $(".mus_cir").animate({ left: `${32 * i}%` });
-    }, 1000);
+    });
   }, 3000);
+
+  // let stop = setInterval(() => {
+  //   if (i > 2) {
+  //     i = 0;
+  //   } else {
+  //     i++;
+  //   }
+  //   musicminute.textContent = `${i}`;
+
+  //   $(".mus_thumb").css({
+  //     transform: "translateX(-30vw)",
+  //     transition: "transform 1s ease",
+  //   });
+
+  //   // 1초 후 실행 (transition 시간과 맞춤)
+  //   setTimeout(() => {
+  //     $(".mus_thumb > li:first-child").appendTo(".mus_thumb");
+  //     $(".mus_thumb").css({
+  //       transform: "translateX(0)",
+  //       transition: "",
+  //     });
+
+  //     // .on 클래스 이동
+  //     $(".mus_thumb > li").removeClass("on");
+  //     $(".mus_thumb > li:nth-child(2)").addClass("on");
+  //     $(".mus_cir").animate({ left: `${32 * i}%` });
+  //   }, 1000);
+  // }, 3000);
 
   $(".pause").on("click", function () {
     console.log("pause");
     $(".pause").textContent = `<i class="fa-solid fa-play"></i>`;
+  });
+
+  $(".con07 li").on("click", function () {
+    $(".modalBox").fadeIn();
+  });
+  $(".close").on("click", function () {
+    $(".modalBox").fadeOut();
   });
 });
