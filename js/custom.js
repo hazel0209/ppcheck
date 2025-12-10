86085a5e42a5c0939b59e0cb3b0a6fde399adae0
hazel0209 <karisk0209@gmail.com>
2025-12-10 16:15:51 +0900

    dev

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,58 @@
-// $(function () {
-//   $(document).on("mousemove", function (e) {
-//     $("#cursor").css({ left: e.clientX + "px", top: e.clientY + "px" });
-//   });
-// });
+$(function () {
+  //   $(document).on("mousemove", function (e) {
+  //     $("#cursor").css({ left: e.clientX + "px", top: e.clientY + "px" });
+  //   });
+
+  //   setInterval(() => {
+  //     $(".skillInfo").animate({ "margin-left": "-20vw -10px" }, function () {
+  //       $(".skillInfo > li:first-child").appendTo(".skillInfo");
+  //       $(".skillInfo").css({ "margin-left": "0" });
+  //     });
+  //   }, 2000);
+
+  //   setInterval(() => {
+  //     $(".mus_thumb").animate({ left: "-30vw" }, 2000, function () {
+  //       $(".mus_thumb > li:first-child").appendTo(".mus_thumb");
+  //       $(".mus_thumb").css({ left: "0" });
+
+  //       $(".mus_thumb > li").removeClass("on");
+  //       $(".mus_thumb > li:nth-child(2)").addClass("on");
+  //     });
+  //   }, 3000);
+
+  let i = 0;
+  let musicminute = document.querySelector(".mus_num");
+
+  let stop = setInterval(() => {
+    if (i > 2) {
+      i = 0;
+    } else {
+      i++;
+    }
+    musicminute.textContent = `${i}`;
+
+    $(".mus_thumb").css({
+      transform: "translateX(-30vw)",
+      transition: "transform 1s ease",
+    });
+
+    // 1초 후 실행 (transition 시간과 맞춤)
+    setTimeout(() => {
+      $(".mus_thumb > li:first-child").appendTo(".mus_thumb");
+      $(".mus_thumb").css({
+        transform: "translateX(0)",
+        transition: "",
+      });
+
+      // .on 클래스 이동
+      $(".mus_thumb > li").removeClass("on");
+      $(".mus_thumb > li:nth-child(2)").addClass("on");
+      $(".mus_cir").animate({ left: `${32 * i}%` });
+    }, 1000);
+  }, 3000);
+
+  $(".pause").on("click", function () {
+    console.log("pause");
+    $(".pause").textContent = `<i class="fa-solid fa-play"></i>`;
+  });
+});
--- js/custom_gsap.js
+++ js/custom_gsap.js
@@ -1,4 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
 
 let v1 = gsap.timeline();
 v1.fromTo(
@@ -28,6 +29,26 @@ v1.fromTo(
     },
     3.5
   )
+  .from(
+    "#visual .lines.a1 hr",
+    {
+      width: "0",
+      opacity: "0",
+      duration: 1,
+      stagger: 0.2,
+    },
+    3.0
+  )
+  .from(
+    "#visual .lines.a2 hr",
+    {
+      width: "0",
+      opacity: "0",
+      duration: 1,
+      stagger: 0.2,
+    },
+    3.0
+  )
   .to(
     ".vis_line",
     {
@@ -51,21 +72,183 @@ v1.fromTo(
       duration: 1,
     },
     4.5
-  )
-  .to(
-    "#visual .imgBox",
-    {
-      width: "100%",
-    },
-    6
   );
 
-gsap.timeline({
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".con02",
+      start: "0% 0%",
+      end: "bottom top",
+      // scrub: 1,
+      pin: true,
+      markers: true,
+    },
+  })
+  .from(".timeBar li", { height: 0, stagger: 0.2 }, 0.6)
+  .from(".timeBar div", { scale: 0, stagger: 0.4 }, 0)
+  .from(".timeline li hr", { width: 0, stagger: 0.5 }, 1.5)
+  .from(".timeline li span", { opacity: 0, y: -40, stagger: 0.4 }, 2)
+  .from(".timeline li p", { opacity: 0, y: -20, stagger: 0.5 }, 2)
+  .from(".github hr", { width: 0 })
+  .from(".github a", { opacity: 0, y: -40 });
+
+let java = gsap.utils.toArray(".java > ul > li");
+let picsub = gsap.utils.toArray(".con07 ul li");
+let sub1 = gsap.utils.toArray(".con07 li.a");
+let sub2 = gsap.utils.toArray(".con07 li.b");
+let sub3 = gsap.utils.toArray(".con07 li.c");
+
+// gsap.to(java, {
+//   scrollTrigger: {
+//     trigger: ".con04",
+//     start: "0% 0%",
+//     end: "+=200%",
+//     scrub: 3,
+//   },
+// })
+// .to(".java > ul li:nth-child(2)", {
+//   scrollTrigger: {
+//     trigger: ".con07",
+//     start: "center center",
+//     scrub: 2,
+//     pin: true,
+//     markers: true,
+//   },
+// });
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".con04",
+      start: "top top",
+      end: "+=200%",
+      scrub: 2,
+      pin: true,
+      markers: true,
+    },
+  })
+  .fromTo(
+    ".java > ul > li:nth-child(2)",
+    {
+      opacity: 0,
+      left: "105%",
+      top: "20%",
+      scale: 0.8,
+    },
+    { opacity: 1, left: 0, top: 0, scale: 1 }
+  )
+  .fromTo(
+    ".mainCirc",
+    {
+      top: -10,
+    },
+    {
+      top: "33.3%",
+    },
+    "<"
+  )
+  .fromTo(
+    ".java > ul > li:nth-child(3)",
+    {
+      opacity: 0,
+      left: "105%",
+      top: "20%",
+      scale: 0.8,
+    },
+    { opacity: 1, left: 0, top: 0, scale: 1 }
+  )
+  .to(
+    ".mainCirc",
+    {
+      top: "66.6%",
+    },
+    "<"
+  )
+  .fromTo(
+    ".java > ul > li:nth-child(4)",
+    {
+      opacity: 0,
+      left: "105%",
+      top: "20%",
+      scale: 0.8,
+    },
+    { opacity: 1, left: 0, top: 0, scale: 1 }
+  )
+  .to(
+    ".mainCirc",
+    {
+      top: "95%",
+    },
+    "<"
+  );
+
+gsap.to(picsub, {
+  x: "-300%",
   scrollTrigger: {
-    trigger: "#visual",
-    start: "50% 50%",
-    end: "bottom top",
-    scrub: 2,
+    trigger: ".con07",
+    start: "0% 0%",
+    end: "+=200%",
+    scrub: 3,
+    pin: true,
     markers: true,
   },
 });
+gsap.to(sub1, {
+  rotation: -10,
+  scrollTrigger: {
+    trigger: ".con07",
+    start: "center center",
+    scrub: 2,
+  },
+});
+gsap.to(sub2, {
+  rotation: 10,
+  scrollTrigger: {
+    trigger: ".con07",
+    start: "center center",
+    scrub: 2,
+  },
+});
+gsap.to(sub3, {
+  rotation: -6,
+  scrollTrigger: {
+    trigger: ".con07",
+    start: "center center",
+    scrub: 2,
+  },
+});
+
+let result1 = "karisk0209@gmail.com";
+let result2 = "메일 전송하기";
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".con08",
+      start: "-10% top",
+      end: "bottom bottom",
+      // scrub: 2,
+      pin: true,
+      markers: true,
+    },
+  })
+  .from(".con08 .left > p:nth-child(1)", { y: -50, opacity: 0 })
+  .from(".con08 .left > p:nth-child(2)", {
+    y: -50,
+    opacity: 0,
+  })
+  .from(".mail i", { opacity: 0 })
+  .to(".mail span", { duration: 1.5, text: result1 }, 1.5)
+  .from(
+    ".right img:nth-child(1)",
+    { duration: 1.5, scale: 0, rotation: -135 },
+    1.5
+  )
+  .from(
+    ".right img:nth-child(2)",
+    { duration: 1.5, scale: 0.8, opacity: 0, rotation: -90 },
+    2.7
+  )
+  .fromTo(".mail a", { y: 15, opacity: 0 }, { y: 0, opacity: 1 })
+  .from(".mail + p", { y: -50, opacity: 0, delay: 0.8 })
+  .from(".mail + p + p", { y: -50, opacity: 0 });
